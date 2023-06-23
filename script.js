780c8a6e4554611b3f26fd090405cdd1192cb1f2
serizelle <sailorelena@gmail.com>
2023-06-23 10:36:48 +0300

    added burger and on scroll sticky menu

---
+++ script.js
@@ -0,0 +1,26 @@
+const footerYearEl = document.querySelector('.footer__year');
+let today = new Date();
+let year = today.getFullYear();
+footerYearEl.textContent = year;
+
+
+const toggle = document.getElementById('toggle')
+const nav = document.getElementById('nav')
+
+toggle.addEventListener('click', () => nav.classList.toggle('active'))
+
+//sticky header
+
+let lastScrollTop = 0;
+const header = document.querySelector('.header__mobile');
+
+window.addEventListener('scroll', () => {
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (scrollTop > lastScrollTop) {
+    // Scrolling down, hide the header
+    header.classList.add('sticky');
+  } 
+
+  lastScrollTop = scrollTop;
+});
